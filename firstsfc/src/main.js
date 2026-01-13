75ea310c4d6b43bb4e11732ed26be419b0ee4a5c
Rodmina Ibe <raibe@student.apc.edu.ph>
2026-01-13 00:50:36 +0000

    Added FoodItem component

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import FoodItem from './components/FoodItem.vue'
+
+const app = createApp(App)
+app.component('food-item', FoodItem)
+app.mount('#app')
